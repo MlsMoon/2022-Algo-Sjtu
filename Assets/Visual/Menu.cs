ba0e6e357e2bc7f0e2da3eb9eae8143c62dc6b54
MlsMoon <1102255102@qq.com>
2022-04-16 21:02:12 +0800

    可视化

---
+++ Assets/Visual/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void OpenMenu(Transform target)
+    {
+        transform.gameObject.SetActive(false);
+        target.gameObject.SetActive(true);
+    }
+
+    public void StartVisualSort()
+    {
+        
+    }
+}
